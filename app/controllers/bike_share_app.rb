0d58840e908d3e0450bcfa740bc43fc8389d6992
James Anderson <james.p.anderson16@gmail.com>
2017-01-27 17:07:54 -0700

    adds partial CRUD function for iteration 1

--- app/controllers/bike_share_app.rb
+++ app/controllers/bike_share_app.rb
@@ -1,3 +1,24 @@
 class BikeShareApp < Sinatra::Base
 
+  get '/' do
+    erb :"home/index"
+  end
+
+  get '/stations' do
+    @stations = Station.all
+    erb :"stations/index"
+  end
+
+  get 'stations/index' do
+  end
+
+  get 'stations/new' do
+    @station = Station.new
+    erb :'stations/new'
+  end
+
+  post '/stations/new' do
+  @station = Station.create(params[:station])
+  redirect "/stations/#{@station.id}"
+  end
 end
