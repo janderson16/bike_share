554ba40f2d036bc2bb67e5976e6350a22de89685
James Anderson <james.p.anderson16@gmail.com>
2017-01-27 18:54:14 -0700

    CRU it. 1 complete, missing the big D

--- app/controllers/bike_share_app.rb
+++ app/controllers/bike_share_app.rb
@@ -5,20 +5,34 @@ class BikeShareApp < Sinatra::Base
   end
 
   get '/stations' do
-    @stations = Station.all
+    @station = Station.all
     erb :"stations/index"
   end
 
-  get 'stations/index' do
-  end
-
-  get 'stations/new' do
+  get '/stations/new' do
     @station = Station.new
     erb :'stations/new'
   end
 
   post '/stations/new' do
-  @station = Station.create(params[:station])
+    @station = Station.create(params[:station])
   redirect "/stations/#{@station.id}"
   end
+
+  get '/stations/:id' do
+      @station = Station.find(params[:id])
+    erb :'stations/show'
+  end
+
+  get '/stations/:id/edit' do
+    @station = Station.find(params[:id])
+    erb :"stations/edit"
+  end
+
+  put '/stations/:id' do
+      @station = Station.update(params[:id], params[:station])
+      redirect "/stations/#{@station.id}"
+    end
+
+
 end
