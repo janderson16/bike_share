1863b7fee3301beb73ead8143764128335189c21
Andy Wooten <andrewdwooten@icloud.com>
2017-01-28 15:58:40 -0700

    completed testing for index view

--- spec/features/user_creates_new_station_spec.rb
+++ spec/features/user_creates_new_station_spec.rb
@@ -1,16 +1,26 @@
 require_relative '../spec_helper'
 
-describe "when a user visits create stations" do
-  it "they can create a new station" do
-    visit "/stations/new"
+  describe "when a user visits '/stations/index' " do
+    it "they see a welcome message" do
 
-    fill_in "station[name]", :with => "Denver"
-    fill_in "station[dock_count]", :with => 111
-    fill_in "station[installation_date]", :with => "11/07/1999"
+        visit("/stations/index")
 
-    click_on("Add New Station")
+        within ("#greeting") do
+          expect(page). to have_content("Stations in Greater San Francisco")
+        end
+    end
 
-    expect(current_path).to eql "/stations/"
-    expect(page).to have_content "Denver"
-  end
-  end
+    # it "they can click on a create station button" do
+    # visit "/"
+    #
+    # click_on "add Stations"
+    # expect(page).to have_current_path "/stations/new"
+    # end
+    #
+    # it "they can click view stations" do
+    # visit "/"
+    #
+    # click_on "View Stations"
+    # expect(page).to have_current_path "/stations/index"
+  # end
+end
