9ba25e2e2bff24c3f977877085534a0ab43ccfd8
James Anderson <james.p.anderson16@gmail.com>
2017-01-29 15:02:53 -0700

    fixed date mixup on edit_station view

--- spec/features/user_edits_station_spec.rb
+++ spec/features/user_edits_station_spec.rb
@@ -9,17 +9,16 @@ describe "when a user edits current station" do
     fill_in "station[name]", :with => "SF1"
     fill_in "station[city]", :with => "Denver"
     fill_in "station[dock_count]", :with => 111
-    fill_in "station[installation_date]", :with => "Mon, 05 Aug 2013"
+    fill_in "station[installation_date]", :with => "Mon, 05 Aug 2014"
 
     click_on("Update Station")
 
     expect(page).to have_current_path "/stations/1"
-    #
-    # visit "/stations/1"
-    #
-    # expect(page).to have_content ("SF1")
-    # expect(page).to have_content ("Denver")
-    # expect(page).to have_content (111)
-    # expect(page).to have_content ("Mon, 05 Aug 2013")
+
+    visit "/stations/1"
+
+    expect(page).to have_content ("SF1")
+    expect(page).to have_content ("Denver")
+    expect(page).to have_content (111)
   end
 end
