0c106d731caac2f4af74d2fcc8522d97e5a572e9
Jonathan Serrano <jonathan.serranor@gmail.com>
2017-01-28 17:36:20 -0700

    adds functionality test for station show

--- spec/features/user_creates_new_station_spec.rb
+++ spec/features/user_creates_new_station_spec.rb
@@ -1,10 +1,9 @@
 require_relative '../spec_helper'
 
-  describe "when a user visits '/stations/index' " do
-    it "they see a welcome message" do
+describe "when a user visits create stations" do
+  it "they can create a new station" do
+    visit "/stations/new"
 
-
-        visit("/stations/index")
     fill_in "station[name]", :with => "Denver"
     #require 'pry'; binding.pry
     fill_in "station[city]", :with => "Denver"
@@ -12,31 +11,10 @@ require_relative '../spec_helper'
     fill_in "station[installation_date]", :with => "11/07/1999"
     station = Station.new(name:"Denver", city:"Denver", dock_count:111, installation_date:"11/07/1999")
 
-
-        within ("#greeting") do
-          expect(page). to have_content("Stations in Greater San Francisco")
-        end
-    end
-
-
-     it "they can click on a create station button" do
-     visit "/"
-    
-     click_on "add Stations"
-     expect(page).to have_current_path "/stations/new"
-     end
-    
-     it "they can click view stations" do
-     visit "/"
-    
-     click_on "View Stations"
-     expect(page).to have_current_path "/stations/index"
-   end
-end
+    click_on("Add New Station")
 
     expect(current_path).to eql "/stations/1"
     expect(page).to have_content "Denver"
     
   end
-  end
-
+end
