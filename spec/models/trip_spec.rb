e254afcb120b34eb20f62d637f2b38af57f076b9
James Anderson <james.p.anderson16@gmail.com>
2017-01-29 16:43:18 -0700

    completes model testing for trips

---
+++ spec/models/trip_spec.rb
@@ -0,0 +1,60 @@
+require "spec_helper"
+
+  RSpec.describe Trip do
+    describe "validations" do
+      it "validates presence of duration" do
+        trip_invalid = Trip.create(start_station: "SF1", start_date: "12/10/2012", end_station: "SF2", end_date: "12/12/2012", bike_id: "12344", subscription_type: "subscriber", zip_code: "12345")
+
+        expect(trip_invalid).to_not be_valid
+      end
+    end
+
+    describe "validations" do
+      it "validates presence of start_station" do
+        trip_invalid = Trip.create(duration: "2 days", start_date: "12/10/2012", end_station: "SF2", end_date: "12/12/2012", bike_id: "12344", subscription_type: "subscriber", zip_code: "12345")
+
+        expect(trip_invalid).to_not be_valid
+      end
+    end
+
+    describe "validations" do
+      it "validates presence of start_date" do
+        trip_invalid = Trip.create(duration: "2 days", start_station: "SF1", end_station: "SF2", end_date: "12/12/2012", bike_id: "12344", subscription_type: "subscriber", zip_code: "12345")
+
+        expect(trip_invalid).to_not be_valid
+      end
+    end
+
+    describe "validations" do
+      it "validates presence of end_station" do
+        trip_invalid = Trip.create(duration: "2 days", start_station: "SF1", start_date: "12/10/2012", end_date: "12/12/2012", bike_id: "12344", subscription_type: "subscriber", zip_code: "12345")
+
+        expect(trip_invalid).to_not be_valid
+      end
+    end
+
+    describe "validations" do
+      it "validates presence of bike_id" do
+        trip_invalid = Trip.create(duration: "2 days", start_station: "SF1", start_date: "12/10/2012", end_station: "SF2", end_date: "12/12/2012", subscription_type: "subscriber", zip_code: "12345")
+
+        expect(trip_invalid).to_not be_valid
+      end
+    end
+
+    describe "validations" do
+      it "validates presence of subscription_type" do
+        trip_invalid = Trip.create(duration: "2 days", start_station: "SF1", start_date: "12/10/2012", end_station: "SF2", end_date: "12/12/2012", bike_id: "12344", zip_code: "12345")
+
+        expect(trip_invalid).to_not be_valid
+      end
+    end
+
+    describe "validations" do
+      it "validates presence of zip_code" do
+        trip_invalid = Trip.create(duration: "2 days", start_station: "SF1", start_date: "12/10/2012", end_station: "SF2", end_date: "12/12/2012", bike_id: "12344", subscription_type: "subscriber")
+
+        expect(trip_invalid).to_not be_valid
+      end
+    end
+
+  end
