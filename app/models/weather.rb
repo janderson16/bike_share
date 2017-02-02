0cd853b76ebf29b9d4a98ec8ef4fe3b608baa542
Spencer Carter <scmountain@gmail.com>
2017-02-02 08:18:47 -0700

    adds last working copy

--- app/models/weather.rb
+++ app/models/weather.rb
@@ -3,20 +3,20 @@ require 'pry'
 class Weather < ActiveRecord::Base
   has_many :trips
 
-  def self.average_number_of_rides(range1, range2)
-    a = where('max_temp > ? AND max_temp < ?', range1, range2)
+  def self.average_number_of_rides
+    a = where('max_temp > ? AND max_temp < ?', 40, 50)
     b = Trip.where(weather_id: a.all.ids)
     b.count / a.count
   end
 
-  def self.max_number_rides_in_temp_range(range1, range2)
-    a = where('max_temp > ? AND max_temp < ?', range1, range2)
+  def self.max_number_rides_in_temp_range
+    a = where('max_temp > ? AND max_temp < ?', 40, 50)
     b = Trip.where(weather_id: a.all.ids)
     b.maximum(:id)
   end
 
-  def self.min_number_rides_in_temp_range(range1, range2)
-    a = where('max_temp > ? AND max_temp < ?', range1, range2)
+  def self.min_number_rides_in_temp_range
+    a = where('max_temp > ? AND max_temp < ?', 40, 50)
     b = Trip.where(weather_id: a.all.ids)
     b.minimum(:id)
   end
@@ -40,38 +40,38 @@ class Weather < ActiveRecord::Base
   #   b.minimum(:id)
   # end
 
-  def self.average_number_of_rides_with_wind(range1, range2)
-    a = where('mean_wind_speed > ? AND mean_wind_speed < ?', range1, range2)
+  def self.average_number_of_rides_with_wind
+    a = where('mean_wind_speed > ? AND mean_wind_speed < ?', 2, 6)
     b = Trip.where(weather_id: a.all.ids)
     b.count / a.count
   end
 
-  def self.max_number_of_rides_with_wind(range1, range2)
-    a = where('mean_wind_speed > ? AND mean_wind_speed < ?', range1, range2)
+  def self.max_number_of_rides_with_wind
+    a = where('mean_wind_speed > ? AND mean_wind_speed < ?', 2, 6)
     b = Trip.where(weather_id: a.all.ids)
     b.maximum(:id)
   end
 
-  def self.min_number_of_rides_with_wind(range1, range2)
-    a = where('mean_wind_speed > ? AND mean_wind_speed < ?', range1, range2)
+  def self.min_number_of_rides_with_wind
+    a = where('mean_wind_speed > ? AND mean_wind_speed < ?', 2, 6)
     b = Trip.where(weather_id: a.all.ids)
     b.minimum(:id)
   end
 
-  def self.average_number_of_rides_with_visibility(range1, range2)
-    a = where('mean_visibility_miles > ? AND mean_visibility_miles < ?', range1, range2)
+  def self.average_number_of_rides_with_visibility
+    a = where('mean_visibility_miles > ? AND mean_visibility_miles < ?', 2, 6)
     b = Trip.where(weather_id: a.all.ids)
     b.count / a.count
   end
 
-  def self.max_number_of_rides_with_visibility(range1, range2)
-    a = where('mean_visibility_miles > ? AND mean_visibility_miles < ?', range1, range2)
+  def self.max_number_of_rides_with_visibility
+    a = where('mean_visibility_miles > ? AND mean_visibility_miles < ?', 2, 6)
     b = Trip.where(weather_id: a.all.ids)
     b.maximum(:id)
   end
 
-  def self.min_number_of_rides_with_visibility(range1, range2)
-    a = where('mean_visibility_miles > ? AND mean_visibility_miles < ?', range1, range2)
+  def self.min_number_of_rides_with_visibility
+    a = where('mean_visibility_miles > ? AND mean_visibility_miles < ?', 2, 6)
     b = Trip.where(weather_id: a.all.ids)
     b.minimum(:id)
   end
