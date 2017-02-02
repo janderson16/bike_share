51346ca189731810acfe087c3764abf2129984ae
Spencer Carter <scmountain@gmail.com>
2017-02-02 04:19:52 -0700

    73 pasing, 8 fails; weather dashboard still div by 0 error

--- app/models/weather.rb
+++ app/models/weather.rb
@@ -21,23 +21,24 @@ class Weather < ActiveRecord::Base
     b.minimum(:id)
   end
 
-  def self.average_number_of_rides_with_precip(range1, range2)
-    a = where('precipitation > ? AND precipitation < ?', 0.0, 0.5)
-    b = Trip.where(weather_id: a.all.ids)
-    b.count / a.count
-  end
-
-  def self.max_number_rides_with_precip(range1, range2)
-    a = where('precipitation > ? AND precipitation < ?', 0.0, 0.5)
-    b = Trip.where(weather_id: a.all.ids)
-    b.maximum(:id)
-  end
-
-  def self.min_number_rides_with_precip(range1, range2)
-    a = where('precipitation > ? AND precipitation < ?', 0.0, 0.5)
-    b = Trip.where(weather_id: a.all.ids)
-    b.minimum(:id)
-  end
+  # def self.average_number_of_rides_with_precip(range1, range2)
+  #   a = Weather.where('precipitation > ? AND precipitation < ?', range1, range2)
+  #   b = Trip.where(weather_id: a.all.ids)
+  #   require "pry"; binding.pry
+  #   b.count / a.count
+  # end
+  #
+  # def self.max_number_rides_with_precip(range1, range2)
+  #   a = Weather.where('precipitation > ? AND precipitation < ?', range1, range2)
+  #   b = Trip.where(weather_id: a.all.ids)
+  #   b.maximum(:id)
+  # end
+  #
+  # def self.min_number_rides_with_precip(range1, range2)
+  #   a = where('precipitation > ? AND precipitation < ?', range1, range2)
+  #   b = Trip.where(weather_id: a.all.ids)
+  #   b.minimum(:id)
+  # end
 
   def self.average_number_of_rides_with_wind(range1, range2)
     a = where('mean_wind_speed > ? AND mean_wind_speed < ?', range1, range2)
