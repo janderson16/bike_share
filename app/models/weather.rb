d88a7393328fc50f7d5d30d87a864575ce21738e
Spencer Carter <scmountain@gmail.com>
2017-02-02 03:40:12 -0700

    adds weather spec testing up to precip issues

--- app/models/weather.rb
+++ app/models/weather.rb
@@ -21,58 +21,58 @@ class Weather < ActiveRecord::Base
     b.minimum(:id)
   end
 
-  # def self.average_number_of_rides_with_precip(range1, range2)
-  #   a = where('precipitation > ? AND precipitation < ?', 0.0, 0.5)
-  #   b = Trip.where(weather_id: a.all.ids)
-  #   b.count / a.count
-  # end
-  #
-  # def self.max_number_rides_with_precip(range1, range2)
-  #   a = where('precipitation > ? AND precipitation < ?', 0.0, 0.5)
-  #   b = Trip.where(weather_id: a.all.ids)
-  #   b.maximum(:id)
-  # end
-  #
-  # def self.min_number_rides_with_precip(range1, range2)
-  #   a = where('precipitation > ? AND precipitation < ?', 0.0, 0.5)
-  #   b = Trip.where(weather_id: a.all.ids)
-  #   b.minimum(:id)
-  # end
-  #
-  # def self.average_number_of_rides_with_wind(range1, range2)
-  #   a = where('mean_wind_speed > ? AND mean_wind_speed < ?', 0, 4)
-  #   b = Trip.where(weather_id: a.all.ids)
-  #   b.count / a.count
-  # end
-  #
-  # def self.max_number_of_rides_with_wind(range1, range2)
-  #   a = where('mean_wind_speed > ? AND mean_wind_speed < ?', 0, 4)
-  #   b = Trip.where(weather_id: a.all.ids)
-  #   b.maximum(:id)
-  # end
-  #
-  # def self.min_number_of_rides_with_wind(range1, range2)
-  #   a = where('mean_wind_speed > ? AND mean_wind_speed < ?', 0, 4)
-  #   b = Trip.where(weather_id: a.all.ids)
-  #   b.minimum(:id)
-  # end
+  def self.average_number_of_rides_with_precip(range1, range2)
+    a = where('precipitation > ? AND precipitation < ?', 0.0, 0.5)
+    b = Trip.where(weather_id: a.all.ids)
+    b.count / a.count
+  end
 
-  # def self.average_number_of_rides_with_visibility(range1, range2)
-  #   a = where('mean_visibility_miles > ? AND mean_visibility_miles < ?', 5, 11)
-  #   b = Trip.where(weather_id: a.all.ids)
-  #   b.count / a.count
-  # end
-  #
-  # def self.max_number_of_rides_with_visibility(range1, range2)
-  #   a = where('mean_visibility_miles > ? AND mean_visibility_miles < ?', 5, 11)
-  #   b = Trip.where(weather_id: a.all.ids)
-  #   b.maximum(:id)
-  # end
-  #
-  # def self.min_number_of_rides_with_visibility(range1, range2)
-  #   a = where('mean_visibility_miles > ? AND mean_visibility_miles < ?', 5, 11)
-  #   b = Trip.where(weather_id: a.all.ids)
-  #   b.minimum(:id)
-  # end
+  def self.max_number_rides_with_precip(range1, range2)
+    a = where('precipitation > ? AND precipitation < ?', 0.0, 0.5)
+    b = Trip.where(weather_id: a.all.ids)
+    b.maximum(:id)
+  end
+
+  def self.min_number_rides_with_precip(range1, range2)
+    a = where('precipitation > ? AND precipitation < ?', 0.0, 0.5)
+    b = Trip.where(weather_id: a.all.ids)
+    b.minimum(:id)
+  end
+
+  def self.average_number_of_rides_with_wind(range1, range2)
+    a = where('mean_wind_speed > ? AND mean_wind_speed < ?', range1, range2)
+    b = Trip.where(weather_id: a.all.ids)
+    b.count / a.count
+  end
+
+  def self.max_number_of_rides_with_wind(range1, range2)
+    a = where('mean_wind_speed > ? AND mean_wind_speed < ?', range1, range2)
+    b = Trip.where(weather_id: a.all.ids)
+    b.maximum(:id)
+  end
+
+  def self.min_number_of_rides_with_wind(range1, range2)
+    a = where('mean_wind_speed > ? AND mean_wind_speed < ?', range1, range2)
+    b = Trip.where(weather_id: a.all.ids)
+    b.minimum(:id)
+  end
+
+  def self.average_number_of_rides_with_visibility(range1, range2)
+    a = where('mean_visibility_miles > ? AND mean_visibility_miles < ?', range1, range2)
+    b = Trip.where(weather_id: a.all.ids)
+    b.count / a.count
+  end
+
+  def self.max_number_of_rides_with_visibility(range1, range2)
+    a = where('mean_visibility_miles > ? AND mean_visibility_miles < ?', range1, range2)
+    b = Trip.where(weather_id: a.all.ids)
+    b.maximum(:id)
+  end
+
+  def self.min_number_of_rides_with_visibility(range1, range2)
+    a = where('mean_visibility_miles > ? AND mean_visibility_miles < ?', range1, range2)
+    b = Trip.where(weather_id: a.all.ids)
+    b.minimum(:id)
+  end
 
 end
--- spec/models/weather_spec.rb
+++ spec/models/weather_spec.rb
@@ -22,69 +22,68 @@ describe ".max_number_rides_in_temp_range" do
       end
     end
 
-  # describe ".average_number_of_rides_with_precip" do
-  #   it "returns .average_number_of_rides_with_precip" do
-  #     setup
-  #     expect(Weather.average_number_of_rides_with_precip(0.0, 0.5)).to eql(1)
-  #     end
-  #   end
-  #
-  #
-  # describe ".max_number_rides_with_precip" do
-  #   it "returns .max_number_rides_with_precip" do
-  #     setup
-  #     expect(Weather.max_number_rides_with_precip(0.0, 0.5)).to eql(1)
-  #     end
-  #   end
-  #
-  # describe ".min_number_rides_with_precip" do
-  #   it "returns .min_number_rides_with_precip" do
-  #     setup
-  #     expect(Weather.min_number_rides_with_precip(0.0, 0.5)).to eql(1)
-  #     end
-  #   end
-  #
-  # describe ".average_number_of_rides_with_wind" do
-  #   it "returns .average_number_of_rides_with_wind" do
-  #     setup
-  #     expect(Weather.average_number_of_rides_with_wind).to eql(1)
-  #     end
-  #   end
-  #
-  # describe ".max_number_of_rides_with_wind" do
-  #   it "returns .max_number_of_rides_with_wind" do
-  #     setup
-  #     expect(Weather.max_number_of_rides_with_wind).to eql(3)
-  #     end
-  #   end
-  #
-  #   describe ".min_number_of_rides_with_wind" do
-  #     it "returns .min_number_of_rides_with_wind" do
-  #       setup
-  #       expect(Weather.min_number_of_rides_with_wind).to eql(1)
-  #       end
-  #     end
-  #
-  #   describe ".average_number_of_rides_with_visibility" do
-  #     it "returns .average_number_of_rides_with_visibility" do
-  #       setup
-  #       expect(Weather.average_number_of_rides_with_visibility).to eql(1)
-  #       end
-  #     end
-  #
-  #   describe ".max_number_of_rides_with_visibility" do
-  #     it "returns .max_number_of_rides_with_visibility" do
-  #       setup
-  #       expect(Weather.max_number_of_rides_with_visibility).to eql(1)
-  #       end
-  #     end
-  #
-  #   describe ".min_number_of_rides_with_visibility" do
-  #     it "returns .min_number_of_rides_with_visibility" do
-  #       setup
-  #       expect(Weather.min_number_of_rides_with_visibility).to eql(1)
-  #       end
-  #     end
+  describe ".average_number_of_rides_with_precip" do
+    it "returns .average_number_of_rides_with_precip" do
+      setup
+      expect(Weather.average_number_of_rides_with_precip(0.0, 0.5)).to eql(1)
+      end
+    end
+
+  describe ".max_number_rides_with_precip" do
+    it "returns .max_number_rides_with_precip" do
+      setup
+      expect(Weather.max_number_rides_with_precip(0.0, 0.5)).to eql(1)
+      end
+    end
+
+  describe ".min_number_rides_with_precip" do
+    it "returns .min_number_rides_with_precip" do
+      setup
+      expect(Weather.min_number_rides_with_precip(0.0, 0.5)).to eql(1)
+      end
+    end
+
+  describe ".average_number_of_rides_with_wind" do
+    it "returns .average_number_of_rides_with_wind" do
+      setup
+      expect(Weather.average_number_of_rides_with_wind(2, 6)).to eql(1)
+      end
+    end
+
+  describe ".max_number_of_rides_with_wind" do
+    it "returns .max_number_of_rides_with_wind" do
+      setup
+      expect(Weather.max_number_of_rides_with_wind(2, 6)).to eql(6)
+      end
+    end
+
+    describe ".min_number_of_rides_with_wind" do
+      it "returns .min_number_of_rides_with_wind" do
+        setup
+        expect(Weather.min_number_of_rides_with_wind(2, 6)).to eql(1)
+        end
+      end
+
+    describe ".average_number_of_rides_with_visibility" do
+      it "returns .average_number_of_rides_with_visibility" do
+        setup
+        expect(Weather.average_number_of_rides_with_visibility(2, 6)).to eql(1)
+        end
+      end
+
+    describe ".max_number_of_rides_with_visibility" do
+      it "returns .max_number_of_rides_with_visibility" do
+        setup
+        expect(Weather.max_number_of_rides_with_visibility(2, 6)).to eql(6)
+        end
+      end
+
+    describe ".min_number_of_rides_with_visibility" do
+      it "returns .min_number_of_rides_with_visibility" do
+        setup
+        expect(Weather.min_number_of_rides_with_visibility(2, 6)).to eql(1)
+        end
+      end
   #
 def setup
   bike = Bike.create(serial_no: "12365")
@@ -93,7 +92,7 @@ def setup
   station = city.stations.create(name: "Santiago", dock_count: 20, installation_date: "11-07-2016")
   weather_1 = Weather.create(max_temp: 59, min_temp: 40, mean_temp: 45, mean_humidity: 50, mean_visibility_miles: 5, mean_wind_speed: 3, precipitation: 1.0, date: "2012-02-04")
   weather_2 = Weather.create(max_temp: 45, min_temp: 41, mean_temp: 50, mean_humidity: 51, mean_visibility_miles: 4, mean_wind_speed: 4, precipitation: 0.5, date: "2012-02-04")
-  weather_3 = Weather.create(max_temp: 41, min_temp: 51, mean_temp: 50, mean_humidity: 51, mean_visibility_miles: 3, mean_wind_speed: 5, precipitation: 0.5, date: "2012-02-04")
+  weather_3 = Weather.create(max_temp: 41, min_temp: 51, mean_temp: 50, mean_humidity: 51, mean_visibility_miles: 3, mean_wind_speed: 5, precipitation: 2.5, date: "2012-02-04")
   weather_4 = Weather.create(max_temp: 44, min_temp: 41, mean_temp: 50, mean_humidity: 51, mean_visibility_miles: 10, mean_wind_speed: 10, precipitation: 1.5, date: "2012-02-04")
   weather_1.trips.create(duration: 90, start_date: "2012-02-03", start_station_id: station.id,
               end_date: "2012-02-04", end_station_id: station.id,
